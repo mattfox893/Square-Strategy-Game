78dc1c3d0c30178c4dc5d58c1fc3b94a3e2b0ec9
Matt Fox <mattfox893@gmail.com>
2022-04-04 14:56:50 -0400

    created grid basics
    gird generate script, tile prefab, moved camera

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int width, height;
+    public Tile tilePrefab;
+
+
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate() {
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < height; z++) {
+                var newTile = Instantiate(tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
+                newTile.name = $"Tile {x} {z}";
+                newTile.transform.parent = this.gameObject.transform;
+            }
+        }
+    }
+}
