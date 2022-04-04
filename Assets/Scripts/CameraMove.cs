0f385ac35a1ad0fa5e2faa77dbc464322e787599
Charlie <charlielu2000@gmail.com>
2022-04-04 16:00:31 -0400

    Update CameraMove.cs
    Camera movement script

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public float speed = 200;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 mousePos = Input.mousePosition;
+
+        // up and down
+        if (mousePos.y >= Screen.height - 10){
+            transform.Translate(speed * Vector3.forward * Time.deltaTime, Space.World);
+        }
+        else if (mousePos.y <= 10){
+            transform.Translate(speed * Vector3.back * Time.deltaTime, Space.World);
+        }
+
+        // left and right
+        if (mousePos.x >= Screen.width - 10)
+        {
+            transform.Translate(speed * Vector3.right * Time.deltaTime, Space.World);
+        }
+        else if (mousePos.x <= 10)
+        {
+            transform.Translate(speed * Vector3.left * Time.deltaTime, Space.World);
+        }
+
+
+
         
     }
 }
