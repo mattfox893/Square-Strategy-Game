6a676b66ff0847dbb460ea4bf3cb496f543960d6
Charlie <charlielu2000@gmail.com>
2022-04-29 21:05:40 -0400

    Update EndTurn.cs

--- Assets/EndTurn.cs
+++ Assets/EndTurn.cs
@@ -10,16 +10,13 @@ public class EndTurn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        endTurn.onClick.AddListener(delegate { TurnManager.Instance.ChangeState(TurnState.EnemyTurn); });
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (TurnManager.Instance.turn == TurnState.PlayerTurn)
-        {
-            endTurn.onClick.AddListener(delegate { TurnManager.Instance.ChangeState(TurnState.EnemyTurn); });
-        }
+
         
     }
 }
