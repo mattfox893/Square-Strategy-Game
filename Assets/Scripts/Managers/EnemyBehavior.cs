a9e7fec9e271ba771bcaf742115253af0e3eaa26
Matt Fox <mattfox893@gmail.com>
2022-05-01 20:26:56 -0400

    revert enemybehavior

--- Assets/Scripts/Managers/EnemyBehavior.cs
+++ Assets/Scripts/Managers/EnemyBehavior.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     bool isMoving;
-    Unit currUnit;
-    Vector3 direction, target, start;
-    Tile startTile;
-    float journeyDist, timeElapsed;
-
+    Unit unit;
+    string dir;
+    
     [SerializeField] float moveSpeed = 4f;
     public static EnemyBehavior Instance;
 
@@ -18,6 +16,7 @@ public class EnemyBehavior : MonoBehaviour
         Instance = this;
     }
 
+<<<<<<< HEAD
     private void Update()
     {
         if (isMoving && currUnit != null)
@@ -27,12 +26,16 @@ public class EnemyBehavior : MonoBehaviour
         }
     }
 
+=======
+>>>>>>> parent of 8fa2510 (updated EnemyBehavior, UnitManager EnemyTurn(), and Unit Death())
     public void Act(Unit unit)
     {
         if (!AttackEnemyWithinRange(unit))
         {
-            for (int i = unit.GetMovement(); i > 0; i--)
+
+            if (unit.GetMovement() > 0 && CheckMoveValid(unit, DetermineClosest(unit)))
             {
+<<<<<<< HEAD
                 if (!isMoving && CheckMoveValid(unit, DetermineClosest(unit)))
                 {
                     //StartCoroutine(Move(unit, DetermineClosest(unit)));
@@ -40,32 +43,14 @@ public class EnemyBehavior : MonoBehaviour
                     currUnit = unit;
                     AttackEnemyWithinRange(unit);
                 }
+=======
+                StartCoroutine(Move(unit, DetermineClosest(unit)));
+                AttackEnemyWithinRange(unit);
+>>>>>>> parent of 8fa2510 (updated EnemyBehavior, UnitManager EnemyTurn(), and Unit Death())
             }
         }
 
-        //Move(unit, "up");
-        //Move(unit, "left");
-
         unit.state = UnitState.Acted;
-    }
-
-    void SetMoving(Unit unit, string dir)
-    {
-        direction = ReturnStringAsDirection(dir);
-        int moveCost = 1;
-        target = unit.transform.position + direction;
-        start = unit.transform.position;
-        startTile = GridManager.GetTile(new Vector2(start.x, start.z), null);
-        journeyDist = 0.1f * moveSpeed;
-
-        if (startTile.GetAttribute() == Attribute.Slow)
-        {
-            moveCost = 2;
-        }
-
-        unit.MoveUnit(moveCost);
-
-        isMoving = true;
     }
 
     // find the closest unit, return a string of their relative direction
@@ -80,19 +65,19 @@ public class EnemyBehavior : MonoBehaviour
             {
                 targetPos = target.GetGridPos();
 
-                if (targetPos.x > unitPos.x && CheckMoveValid(unit, "right"))
+                if (targetPos.x > unitPos.x)
                 {
                     dir = "right";
                 }
-                else if (targetPos.x < unitPos.x && CheckMoveValid(unit, "left"))
+                else if (targetPos.x < unitPos.x)
                 {
                     dir = "left";
                 }
-                else if (targetPos.y > unitPos.y && CheckMoveValid(unit, "up"))
+                else if (targetPos.y > unitPos.y)
                 {
                     dir = "up";
                 }
-                else if (targetPos.y < unitPos.y && CheckMoveValid(unit, "down"))
+                else if (targetPos.y < unitPos.y)
                 {
                     dir = "down";
                 }
@@ -156,15 +141,8 @@ public class EnemyBehavior : MonoBehaviour
         return direction;
     }
 
-    void Move(Unit unit, string dir)
-    {
-        /*Vector3 direction = ReturnStringAsDirection(dir);
-        int moveCost = 0;
-        Vector3 target = unit.transform.position + direction;
-        Vector3 start = unit.transform.position;
-        Tile startTile = GridManager.GetTile(new Vector2(start.x, start.z), null);
-        Tile targetTile = GridManager.GetTile(new Vector2(target.x, target.z), null);*/
 
+<<<<<<< HEAD
         if (timeElapsed <= journeyDist)
         {
             // unit.transform.position += (target - start) * moveSpeed * Time.deltaTime;
@@ -182,6 +160,8 @@ public class EnemyBehavior : MonoBehaviour
     }
 
 /*
+=======
+>>>>>>> parent of 8fa2510 (updated EnemyBehavior, UnitManager EnemyTurn(), and Unit Death())
     IEnumerator Move(Unit unit, string dir)
     {
         isMoving = true;
@@ -215,7 +195,7 @@ public class EnemyBehavior : MonoBehaviour
         unit.transform.position = new Vector3(Mathf.Round(unit.transform.position.x), unit.transform.position.y, Mathf.Round(unit.transform.position.z));
         isMoving = false;
     }
-*/
+
 
     IEnumerator Wait(float t)
     {
